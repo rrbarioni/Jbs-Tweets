fadd9dd840c1e9dfe5fb5de040e95e905861d7c6
Ricardo Rossiter Barioni <rrb@cin.ufpe.br>
2017-11-27 08:22:54 -0300

    working with csv

--- tweets_representation.py
+++ tweets_representation.py
@@ -1,5 +1,6 @@
 import re
 import os
+import csv
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -11,6 +12,33 @@ def clean_tweet(tweet):
 	Lower case letters
 	'''
 	return re.sub('[^a-zA-Z]+', ' ', tweet).lower()
+
+def train():
+	tweets = []
+
+	with open('miranda/caveira.txt', encoding='utf-8') as txt_file:
+		tweets = [clean_tweet(line.strip()) for line in txt_file]
+		print(tweets[0])
+
+def write_to_csv():
+	tweets = []
+	tweets_files_folder = 'tweets_1000_day/raw'
+	tweets_file_list = os.listdir(tweets_files_folder)
+
+	with open("all_clean_tweets.csv", "w", newline="") as tweets_csv:
+		spamwriter = csv.writer(tweets_csv, delimiter=';')
+		for file_name in tweets_file_list:
+			with open(tweets_files_folder + '/' + file_name, encoding='utf-8') as txt_file:
+				current_file_date = file_name[4:14]
+				tweets = [clean_tweet(line.strip()) for line in txt_file]
+				for tweet in tweets:
+					spamwriter.writerow([str(current_file_date), tweet.replace(';', ',')])
+
+def main_2():
+	with open('tweets_1000_day/all_clean_tweets.csv', newline='') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+		for row in spamreader:
+			print(row)
 
 def main():
 	tweets = []
@@ -24,10 +52,6 @@ def main():
 		with open(tweets_files_folder + '/' + file_name, encoding='utf-8') as txt_file:
 			tweets += [clean_tweet(line.strip()) for line in txt_file]
 			print(str(file_name) + ", " + str(len(tweets)))
-
-	# with open('miranda/caveira.txt', encoding='utf-8') as txt_file:
-	# 	tweets = [clean_tweet(line.strip()) for line in txt_file]
-	# 	print(tweets[0])
 
 	# Bag of words
 	count_vect = CountVectorizer()
@@ -44,4 +68,5 @@ def main():
 	# clf = MultinomialNB().fit(x_train_tfidf, twenty_train.target)
 
 if __name__ == '__main__':
-	main()
+	main_2()
+	# write_to_csv()
